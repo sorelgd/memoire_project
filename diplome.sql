cfab7f8930564f4c3aa6bae33676983768688256
sorelgd <35955371+sorelgd@users.noreply.github.com>
2018-05-27 08:50:25 +0100

    Liste 1 diplome

---
+++ diplome.sql
@@ -0,0 +1,17 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+INSERT INTO `diplome` (`id`, `codeDiplome`, `nameDiplome`, `isactive`) VALUES
+(1,'CAP','Certificat d\'aptitude Professionnelle',1),
+(2,'DT','Diplôme de Technicien',1),
+(3,'BAC','Baccalauréat',1),
+(4,'BTS','Brevet du Technicien Supérieur',1),
+(5,'Lic','Licence',1),
+(6,'Mas','Master',1),
+(7,'Ing','Ingénieur de conception',1),
+(8,'Dr','Docteur',1),
+(9,'DEA','Diplôme d\'Etude Approfondie',1),
+(10,'BAPET','BAPET',1),
+(11,'CAPET','CAPET',1);
