cf999f6477b23708d4b60ccd0570a9f8bdc420e9
sorelgd <35955371+sorelgd@users.noreply.github.com>
2018-05-27 08:17:42 +0100

    Liste 1 des domaines

---
+++ Domaine.sql
@@ -0,0 +1,22 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+INSERT INTO `domaine` (`id`, `codeDomaine`, `nameDomaine`, `isactive`) VALUES
+(1,'01','Arts, Philosophie et Sociologie',1),
+(2,'02','Communication et Journalisme',1),
+(3,'03','Enseignement',1),
+(4,'04','Informatique et Télécommunications',1),
+(5,'05','Sport',1),
+(6,'06','Biologie et Médecine',1),
+(7,'07','Droit et Sciences Politiques',1),
+(8,'08','Géographie',1),
+(9,'09','Ressources humaines',1),
+(10,'10','Hôtellerie et Tourisme',1),
+(11,'11','Commerce et Marketing',1),
+(12,'12','Economie et Finance',1),
+(13,'13','Histoire',1),
+(14,'14','Sciences',1),
+(15,'15','Rapports de stage',1),
+(16,'16','Santé',1);
